2ebdfe1e2adf2cfefd6b45e3e6b327e997d8a458
Werewolf-44 <133239997+Werewolf-44@users.noreply.github.com>
2023-07-31 10:26:24 +0200

    Add files via upload

--- 1. quiz_game.py
+++ 1. quiz_game.py
@@ -1,16 +1,16 @@
 #Simply Quiz game. Writing down last names of famous people. At the end score is printed out.
 
-print("Welcome to 'Famous People' quiz! You need to write their last name. Are you ready? Type 'yes' or 'no'")
+print("Welcome to 'Famous People' quiz! You need to write their last name")
 
 
 while True:
-    playing = input("Do you want to play 'Famous people' game? ")
+    playing = input("Do you want to play 'Famous people' game? Type 'yes' or 'no' ")
 
     if playing.lower() == "yes":
         print("Great, let's move on to the first question!")
         break
     elif playing.lower() == "no":
-        print("Sad to hear that... Well, maybe next time! Game ends here")
+        print("Sad to hear that...Well, maybe next time! Game ends here")
         exit()
     else:
         print("Invalid answer. Type 'yes' or 'no'. Please try again")
@@ -20,35 +20,38 @@ score = 0
 
 answer = input("First question: His name is Brad? ")
 if answer.lower() == "pitt":
-    print("Correct!")
+    print("Brad {}. That is correct!".format(answer.capitalize()))
     score += 1
 else: 
     print("Incorrect!")
 
 answer = input("Second question: Her name is Angelina? ")
 if answer.lower() == "jolie":
-    print("Correct!")
+    print("Angelina {}. That is correct!".format(answer.capitalize()))
     score += 1
 else: 
     print("Incorrect!")
 
 answer = input("His name is Johny? ")
 if answer.lower() == "depp":
-    print("Correct!")
+    print("Johnny {}. That is correct!".format(answer.capitalize()))
     score += 1
 else: 
     print("Incorrect!")
 
 answer = input("His name is Orlando? ")
 if answer.lower() == "bloom":
-    print("Correct!")
+    print("Orlando {}. That is correct!".format(answer.capitalize()))
     score += 1
 else: 
     print("Incorrect!")
 
 answer = input("Her names is Catherine Zeta- ")
 if answer.lower() == "jones":
+    print("Catherine {}. That is correct!".format(answer.capitalize()))
     score +=1
+else:
+    print("Incorrect!")
 
 print("You got " + str(score) + " questions correct! That is " + str((score/5) * 100) + "%.")
 print("Thank you for playing the game!")
